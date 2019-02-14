bbb2a39c7da5dbef74af3c27931aa78183c92e3a
QuimLaz <quimlaz@gmail.com>
2019-02-14 17:47:21 +0100

    started gallerie, falta modal carousel

---
+++ JS/modal.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+	document.getElementById("carouselExampleControls").style.display="none";
+} 
+
+function showCarousel {
+	document.getElementById("carouselExampleControls").style.display="block";
+}
